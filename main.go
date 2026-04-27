3d78544a5aef1e8a3948e2553b6ecc0e67b5d968
shaik asif <smd.20sa@gmail.com>
2026-04-27 15:56:34 +0530

    dafer

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	sum := 0
-	i := 0
-	for i < 10 {
-		sum += i
-		i++
+	fmt.Println("start")
 
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
 	}
-	fmt.Println(sum)
+	fmt.Println("end")
 
 }
